875e95cadd4e52b887fef1a6a04f87cccd2dd4eb
sendott <sendibagaskara7@gmail.com>
2020-03-02 16:26:33 +0700

    Initial commit

---
+++ app/src/main/java/riko/sendi/myapp/MainActivity.kt
@@ -0,0 +1,12 @@
+package riko.sendi.myapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
